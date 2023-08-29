8424fcd1418ddccebf66f7edc9f09bf7c914e0bc
mariquegonn-dev <ocatodev@gmail.com>
2023-08-29 15:54:52 -0300

    config: safeList for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    {
+      pattern: /^bg-/,
+      variants: ["primaryYellow", "primaryBlue"],
+    },
+    {
+      pattern: /^hover:bg-/,
+      variants: ["secundaryYellow", "secundaryBlue"],
+    },
+  ],
   theme: {
     extend: {
       backgroundImage: {

6bf6234a439ed1266996ecf6981e8b28ddc8279b
mariquegonn-dev <ocatodev@gmail.com>
2023-08-29 15:43:46 -0300

    feat: hook useMedia created

---
+++ src/app/hooks/useMedia/useMedia.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useMedia = (media: string) => {
+  const [match, setMatch] = useState(false);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener("resize", changeMatch);
+    return () => window.removeEventListener("resize", changeMatch);
+  }, [media]);
+  return match;
+};

15ff4eacdb4d0cbb5fc004a522604ee1c5fea72c
mariquegonn-dev <ocatodev@gmail.com>
2023-09-01 17:51:08 -0300

    feat: Whatsapp Button created

---
+++ src/app/components/Modal/Whatsapp/Whatsapp.tsx
@@ -0,0 +1,11 @@
+import { WhatsappIcon } from "../../Icons";
+
+export const Whatsapp = () => {
+  return (
+    <>
+      <div className="animate-pulseAnimation fixed bottom-10 right-10 z-[9999] rounded-full bg-green-400 p-2">
+        <WhatsappIcon size={32} color="#f7f7f7" />
+      </div>
+    </>
+  );
+};

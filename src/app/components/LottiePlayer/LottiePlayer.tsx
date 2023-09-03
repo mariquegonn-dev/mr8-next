26932ff9a43f1b3cadf8276b37173e4e7edb73ab
mariquegonn-dev <ocatodev@gmail.com>
2023-09-02 21:01:54 -0300

    feat: loading lottiePlayer

--- src/app/components/LottiePlayer/LottiePlayer.tsx
+++ src/app/components/LottiePlayer/LottiePlayer.tsx
@@ -3,26 +3,40 @@
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 import { useMedia } from "@/app/hooks/useMedia";
+import { useState } from "react";
+
+import "./loading.css";
 
 type LottiePlayerProps = {
   src: object;
 };
 
 export const LottiePlayer = ({ src }: LottiePlayerProps) => {
+  const [loading, setLoading] = useState(true);
   const sizeScreen = useMedia("(max-width:640px)");
 
   const ternaryScreen = `${sizeScreen ? "250px" : "400px"}`;
+
   return (
-    <Player
-      autoplay
-      loop
-      src={src}
-      style={{
-        height: ternaryScreen,
-        width: ternaryScreen,
-      }}
-    >
-      <Controls visible={false} />
-    </Player>
+    <>
+      {loading && <div className="loader"></div>}
+
+      <Player
+        onEvent={(event) => {
+          if (event !== "load") {
+            setLoading(false);
+          }
+        }}
+        autoplay
+        loop
+        src={src}
+        style={{
+          height: loading ? "0px" : ternaryScreen,
+          width: loading ? "0px" : ternaryScreen,
+        }}
+      >
+        <Controls visible={false} />
+      </Player>
+    </>
   );
 };
